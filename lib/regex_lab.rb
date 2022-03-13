98c0414715561b0da0ddb25dbcc8d1c829358d0e
Heidi Hlavinka <hchlav@gmail.com>
2022-03-13 17:17:44 -0500

    commit tree 7d6f1b73a9b1b9e28209e2e6b8a3d6f69fb73fb2

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
-def starts_with_a_vowel?(word)
+require 'pry'
+# binding.pry
 
+def starts_with_a_vowel?(word)
+  word.match? /\A[aeiou]/i
 end
+
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing/)
 end
+
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
+
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match?(/\b[A-Z]\b/)
 end
+
 
 def valid_phone_number?(phone)
+  phone.match?(/(\d{10})|(\(\d{3}\)\d{3}-\d{4})|\d{3} \d{3} \d{4}|(\(\d{3}\)\d{7})/)
 
 end
+# binding.pry
